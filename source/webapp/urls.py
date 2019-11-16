4495d0992ee6a720c5d9421d8cdf2fb7f03cda8d
Ваше Имя <you@example.com>
2019-11-16 13:18:55 +0600

    done stage 2

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProductList
+from .views import ProductList, ProductDetails, ProductEdit, ProductDelete
 
 app_name = 'webapp'
 
 urlpatterns = [
-    path('', ProductList.as_view(), name='main_url')
+    path('', ProductList.as_view(), name='main_url'),
+    path('details/<int:pk>/', ProductDetails.as_view(), name='product_details_url'),
+    path('edit/<int:pk>/', ProductEdit.as_view(), name='product_edit_url'),
+    path('delete/<int:pk>/', ProductDelete.as_view(), name='product_delete_url')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+
+from webapp.models import Product
 
 
-class ProductList(View):
-    def get(self, *args, **kwargs):
-        return render(self.request, 'base.html')
+class ProductList(ListView):
+    model = Product
+    template_name = 'product_index.html'
+    context_object_name = 'product_list'
+
+
+class ProductDetails(DetailView):
+    model = Product
+    template_name = 'product_details.html'
+    context_object_name = 'product'
+
+
+class ProductEdit(UpdateView):
+    model = Product
+    template_name = 'product_edit.html'
+    fields = ['image', 'name', 'category', 'description']
+    success_url = reverse_lazy('webapp:main_url')
+
+
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = 'product_delete.html'
+    success_url = reverse_lazy('webapp:main_url')
