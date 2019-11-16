393ee6b875abbb8a9237efc8c9ded587894879a8
Ваше Имя <you@example.com>
2019-11-16 12:24:38 +0600

    done stage 1

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Product, Review
+
+admin.site.register(Product)
+admin.site.register(Review)
+
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+PRODUCT_CATEGORY = (
+    ('fruits', "Фрукты"),
+    ("wear", "Одежда"),
+    ("other", "Другое")
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=63, verbose_name='Название')
+    category = models.CharField(max_length=63, choices=PRODUCT_CATEGORY, default=PRODUCT_CATEGORY[2][0], verbose_name='Категории')
+
+    description = models.TextField(max_length=300, verbose_name='Описание', null=True, blank=True)
+    image = models.ImageField(upload_to='product_images', null=True, blank=True, verbose_name='Картинка товара')
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User,  verbose_name='Автор', related_name='review_user', on_delete=models.PROTECT)
+    product = models.ForeignKey('Product', related_name='review_product', verbose_name='Товар', on_delete=models.CASCADE)
+    review_text = models.TextField(max_length=511, verbose_name='Текст отзыва')
+    mark = models.PositiveIntegerField(default=5,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ])
+
+
